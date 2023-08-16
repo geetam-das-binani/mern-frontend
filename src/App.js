9a906a5066fdbada817c2ce968020a126a4bb845
Geetam <geetambinani6@gmail.com>
2023-08-16 18:33:45 +0530

    header component done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import React, { useEffect } from 'react'
+import Header from './component/layout/Header'
+import webFont from 'webfontloader'
+
+
+export default function App() {
+    useEffect(()=>{
+        webFont.load({
+            google:{
+                families:['Roboto Mono', 'monospace']
+            }
+        })
+        },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Header />
+  )
 }
 
-export default App;
+
+
